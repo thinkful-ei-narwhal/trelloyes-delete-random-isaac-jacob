6c0edb4af9676cf66752f14e0f8b72dd59bbd102
Kevin Jiang <kjiang95@gmail.com>
2020-04-06 14:05:34 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import Card from './Card';
 import List from './List';
+import './App.css';
 
 class App extends React.Component {
 	static defaultProps = {
--- src/List.js
+++ src/List.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from './Card';
+import './List.css';
 
 const List = (props) => {
 	return (
 		<section className='List'>
 			<header className='List-header'>
-				<h2>props.header</h2>
+				<h2>{props.header}</h2>
 			</header>
 
 			<div className='List-cards'>
